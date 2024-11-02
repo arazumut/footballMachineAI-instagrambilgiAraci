782d30b87c7301359846db7e99e2ab1615802c89
K.Umut Araz <150933483+arazumut@users.noreply.github.com>
2024-11-02 21:11:51 +0300

    Add to some functions footballmachine.py

--- footballmachine.py
+++ footballmachine.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-
 veri = pd.read_csv('understat_per_game.csv')
 
 
